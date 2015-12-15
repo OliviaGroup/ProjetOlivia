28bf99dc45650a53baf3562b291f0c0e48d8c243
Roos Quentin <quentin.roos@viacesi.fr>
2015-12-15 13:55:48 +0100

    dans la création de nouveau profils changement de l'age en date de naissance (partie graphique OK code behind non fonctionnel pour le moment

--- app/src/main/java/com/principal/projetolivia/main/NewProfileFragment.java
+++ app/src/main/java/com/principal/projetolivia/main/NewProfileFragment.java
@@ -1,5 +1,6 @@
 package com.principal.projetolivia.main;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -7,12 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
+import com.gc.materialdesign.views.ButtonFlat;
 import com.gc.materialdesign.views.ButtonRectangle;
 import com.gc.materialdesign.widgets.SnackBar;
 import com.principal.projetolivia.R;
 import com.principal.projetolivia.com.principal.projetolivia.util.User;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -21,8 +29,10 @@ public class NewProfileFragment extends Fragment {
 
     private EditText textName;
     private EditText textAge;
+    private Calendar myCalendar = Calendar.getInstance();
 
     private ButtonRectangle buttonValidation;
+    private ButtonFlat buttonChangeDate;
 
     public NewProfileFragment() {
     }
@@ -34,6 +44,33 @@ public class NewProfileFragment extends Fragment {
 
         textName = (EditText) rootView.findViewById(R.id.textNameProfile);
         textAge = (EditText) rootView.findViewById(R.id.textAgeProfile);
+        textAge.setKeyListener(null);
+
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+        buttonChangeDate = (ButtonFlat) rootView.findViewById(R.id.buttonChangeDate);
+
+        buttonChangeDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(rootView.getContext(), date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH));
+
+                DatePicker datePicker = datePickerDialog.getDatePicker();
+
+                datePicker.setMaxDate(Calendar.getInstance().getTimeInMillis());
+                datePicker.setCalendarViewShown(false);
+
+                datePickerDialog.show();
+            }
+        });
 
         buttonValidation = (ButtonRectangle) rootView.findViewById(R.id.buttonValidation);
         buttonValidation.setOnClickListener(new View.OnClickListener() {
@@ -54,7 +91,7 @@ public class NewProfileFragment extends Fragment {
 
                     View view = getActivity().getCurrentFocus();
                     if (view != null) {
-                        InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(getActivity().INPUT_METHOD_SERVICE);
+                        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(getActivity().INPUT_METHOD_SERVICE);
                         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
                     }
 
@@ -68,5 +105,13 @@ public class NewProfileFragment extends Fragment {
 
 
         return rootView;
+    }
+
+    private void updateLabel() {
+
+        String myFormat = "dd/MM/yy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(myFormat, Locale.getDefault());
+
+        textAge.setText(simpleDateFormat.format(myCalendar.getTime()));
     }
 }
